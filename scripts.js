86b73eda8d6d8322b59f534baafea0b481cecc6f
klanza <klanza91@gmail.com>
2018-07-15 20:44:19 -0700

    Play button updating on click

--- scripts.js
+++ scripts.js
@@ -20,39 +20,8 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
-function skip() {
-  video.currentTime += parseFloat(this.dataset.skip);
-}
-
-function handleProgress() {
-  const percent = (video.currentTime / video.duration) * 100;
-  progressBar.style.flexBasis = `${percent}%`;
-}
-
-function handleRangeUpdate() {
-  video[this.name] = this.value;
-  // console.log(this.name);
-  // console.log(this.value);
-}
-
-function scrub(e) {
-  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-  video.currentTime = scrubTime;
-}
-
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
-
-toggle.addEventListener('click', togglePlay);
-skipButtons.forEach(button => button.addEventListener('click', skip));
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
-
-let mouseclick = false;
-progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', e => mouseclick && scrub(e));
-progress.addEventListener('mousedown', () => (mouseclick = true));
-progress.addEventListener('mouseup', () => (mouseclick = false));
